5ac0190d66e2623c77c4524adac447382558e91d
jamipouchi <miquelpuigturon@gmail.com>
2023-02-26 16:08:53 +0100

    main functionalities built. Checked working without real testing. Checked efficiency without benchmarking. Looking good.

--- paper_reproduction.cpp
+++ paper_reproduction.cpp
@@ -1,17 +1,82 @@
 #include <iostream>
 #include <random>
 #include <vector>
+#include <unordered_set>
 #include <tuple>
 
 std::random_device rd;  // Only used once to initialise (seed) engine
 std::mt19937 rng(rd()); // Random-number engine used (Mersenne-Twister in this case)
 
-struct Graph
+// TODO: MOVE TO PRIVATE EDGES AND NON_EDGES
+
+class Graph
 {
-    std::vector<std::vector<int>> edges;
-    std::vector<std::vector<int>> non_edges;
+private:
     int edge_count;
-    Graph(int node_count) : edge_count(0), edges(node_count, std::vector<int>()), non_edges(node_count, std::vector<int>(node_count - 1))
+
+    static std::tuple<int, int> find_link(std::vector<std::unordered_set<int>> &edges, int possibilities)
+    {
+        std::uniform_int_distribution<int> uni(0, possibilities - 1); // Guaranteed unbiased
+        int index_to_delete = uni(rng);
+        int node = 0;
+        int link_count = 0;
+        while (link_count + edges[node].size() <= index_to_delete)
+        {
+            link_count += edges[node].size();
+            node++;
+        };
+
+        return std::make_tuple(node, *std::next(edges[node].begin(), index_to_delete - link_count)); // edges[node][index_to_delete - link_count]);
+    }
+
+    static void add_link(std::vector<std::unordered_set<int>> &edges, int from, int to)
+    {
+        edges[from].insert(to);
+        edges[to].insert(from);
+    }
+
+    static void remove_link(std::vector<std::unordered_set<int>> &edges, int from, int to)
+    {
+        edges[from].erase(to);
+        edges[to].erase(from);
+    }
+
+    std::tuple<int, int> choose_random_non_existing_link()
+    {
+        int n = this->edges.size();
+        int possibilities = 2 * ((n * (n - 1) / 2) - (this->edge_count));
+        if (possibilities == 0)
+        {
+            throw std::runtime_error("There's no missing links in a complete network");
+        }
+        return find_link(this->non_edges, possibilities);
+    }
+
+    std::tuple<int, int> choose_random_existing_link()
+    {
+        int possibilities = 2 * this->edge_count;
+        if (possibilities == 0)
+        {
+            throw std::runtime_error("There's no links in an empty network");
+        }
+        return find_link(this->edges, possibilities);
+    }
+
+    void remove_all_node_links(int node)
+    {
+        this->edge_count -= this->edges[node].size();
+        for (int to : this->edges[node])
+        {
+            add_link(this->non_edges, node, to);
+            this->edges[to].erase(node);
+        }
+        this->edges[node].clear();
+    }
+
+public:
+    std::vector<std::unordered_set<int>> edges;
+    std::vector<std::unordered_set<int>> non_edges;
+    Graph(int node_count) : edge_count(0), edges(node_count, std::unordered_set<int>()), non_edges(node_count, std::unordered_set<int>(node_count - 1))
     {
         for (int node = 0; node < node_count; node++)
         {
@@ -20,112 +85,63 @@ struct Graph
             {
                 if (edge != node)
                 {
-                    non_edges[node][connection] = edge;
+                    non_edges[node].insert(edge);
                 }
                 else
                 {
                     edge++;
-                    non_edges[node][connection] = edge;
+                    non_edges[node].insert(edge);
                 }
                 edge++;
             }
         }
     }
-};
 
-std::tuple<int, int> find_link(std::vector<std::vector<int>> &edges, int possibilities)
-{
-    std::uniform_int_distribution<int> uni(0, possibilities); // Guaranteed unbiased
-    int index_to_delete = uni(rng);
-    int node = 0;
-    int link_count = 0;
-    while (link_count + edges[node].size() < index_to_delete)
+    void add_random_link()
     {
-        link_count += edges[node].size();
-        node++;
-    };
+        auto [from, to] = this->choose_random_non_existing_link();
 
-    return std::make_tuple(node, edges[node][index_to_delete - link_count]);
-}
-
-std::tuple<int, int> choose_random_existing_link(Graph *g)
-{
-    int possibilities = 2 * g->edge_count;
-    return find_link(g->edges, possibilities);
-}
-
-std::tuple<int, int> choose_random_non_existing_link(Graph *g)
-{
-    int n = g->edges.size();
-    int possibilities = (n * (n - 1) / 2) - (2 * g->edge_count);
-    return find_link(g->non_edges, possibilities);
-}
-
-void add_link(std::vector<std::vector<int>> &edges, int from, int to)
-{
-    edges[from].push_back(to);
-    edges[to].push_back(from);
-}
-
-void remove_link(std::vector<std::vector<int>> &edges, int from, int to)
-{
-    edges[from].erase(edges[from].begin() + to);
-    edges[to].erase(edges[to].begin() + from);
-}
-
-void add_random_link(Graph *g)
-{
-    auto [from, to] = choose_random_non_existing_link(g);
-
-    add_link(g->edges, from, to);
-    remove_link(g->non_edges, from, to);
-}
-
-void remove_random_link(Graph *g)
-{
-    auto [from, to] = choose_random_existing_link(g);
-
-    add_link(g->non_edges, from, to);
-    remove_link(g->edges, from, to);
-}
-
-void remove_all_node_links(Graph *g, int node)
-{
-    for (int to : g->edges[node])
-    {
-        add_link(g->non_edges, node, to);
-        g->edges[to].erase(g->edges[to].begin() + node);
+        this->edge_count += 1;
+        add_link(this->edges, from, to);
+        remove_link(this->non_edges, from, to);
     }
-    g->edges[node].clear();
-}
 
-void remove_all_links_from_random_node(Graph *g)
-{
-    std::uniform_int_distribution<int> uni(0, g->edges.size()); // Guaranteed unbiased
-    int node = uni(rng);
-    remove_all_node_links(g, node);
-}
+    void remove_random_link()
+    {
+        auto [from, to] = this->choose_random_existing_link();
+
+        this->edge_count -= 1;
+        add_link(this->non_edges, from, to);
+        remove_link(this->edges, from, to);
+    }
+
+    void remove_all_links_from_random_node()
+    {
+        std::uniform_int_distribution<int> uni(0, this->edges.size() - 1); // Guaranteed unbiased
+        int node = uni(rng);
+        this->remove_all_node_links(node);
+    }
+};
 
 int main()
 {
     int node_count;
-    std::cout << "Input number of nodes" << std::endl;
     std::cin >> node_count;
     Graph *g = new Graph(node_count);
 
+    /**
     int simulation_steps;
-    std::cout << "Input number of simulation_steps" << std::endl;
     std::cin >> simulation_steps;
-
     for (int step = 0; step < simulation_steps; step++)
     {
-        if (step % alpha_freq == 0)
+        if (step % alpha_period == 0)
         {
-            add_random_link(g);
+            g->add_random_link();
         }
-        if (step % r_freq == 0)
+        if (step % r_period == 0)
         {
-            remove_all_links_from_random_node(g);
+            g->remove_all_links_from_random_node();
         }
     }
+    **/
 }
